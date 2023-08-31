5039fce5c871569e0fa29d9c06ac73fdd7dd5c95
Michael Chilaka <obichilaka@gmail.com>
2023-08-31 15:36:01 +0100

    wrapped the app in redux provider

--- multi-step-form/src/index.js
+++ multi-step-form/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
-import store from "./"
+import { store } from './store';
 
 import './index.css';
 import App from './App';
@@ -11,7 +11,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
