5b8da3d6739e391085265301e63352b735356be0
Michael Chilaka <obichilaka@gmail.com>
2023-08-31 15:32:38 +0100

    step5 ui updates completed

--- multi-step-form/src/index.js
+++ multi-step-form/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import store from "./"
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
---
+++ multi-step-form/src/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import nameReducer from "./services/nameSlice";
+import emailReducer from "./services/emailSlice";
+import phoneReducer from "./services/phoneSlice";
+import addOnsReducer from "./services/addOnsSlice";
+import billingOptionReducer from "./services/billingOptionSlice";
+import billingTypeReducer from "./services/billingTypeSlice";
+
+export const store = configureStore({
+    reducer: {
+        name: nameReducer,
+        email: emailReducer,
+        phone: phoneReducer,
+        billingOption: billingOptionReducer,
+        billingType: billingTypeReducer,
+        addOns: addOnsReducer,
+    }
+})
